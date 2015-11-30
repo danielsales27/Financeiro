05283be8dcc203f2e988d0f0b01bec0b6c16d513
Daniel Sales Silva <Daniel Sales Silva>
2015-11-29 23:27:52 -0300

    Changes CRUD Knockout

---
+++ Financeiro.Web/Scripts/CRUD/Despesa.js
@@ -0,0 +1,36 @@
+﻿
+function DespesaViewModel() {
+
+    var self = this;
+
+    self.Despesa_Id = ko.observable("");
+    self.Nome = ko.observable("");
+    self.Valor = ko.observable("");
+    self.Categoria = ko.observable("");
+    self.Data = ko.observable("");
+    self.Observacao = ko.observable("");
+
+    var Despesa = {
+        Despesa_Id: self.Despesa_Id,
+        Nome: self.Nome,
+        Valor: self.Valor,
+        Categoria: self.Categoria,
+        Data: self.Data,
+        Observacao: self.Observacao
+    };
+
+    self.Despesa = ko.observable();
+    self.Despesas = ko.observableArray();
+
+    $.ajax({
+        url: '@Url.Action("Index", "Despesas")',
+        cache: false,
+        type: 'GET',
+        contentType: 'application/json; charset=utf-8',
+        data: {},
+        success: function (data) {
+            self.Despesas(data);
+        }
+    });
+
+}
